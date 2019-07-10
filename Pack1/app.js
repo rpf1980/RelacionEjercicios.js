3acfab1b18a20fd6ea7ca44e4df70f9c1505d402
rpf1980 <united_ssh@outlook.com>
2019-07-10 11:58:29 +0200

    Ejercicio 2 casi acabado

--- Pack1/app.js
+++ Pack1/app.js
@@ -1,1 +1,40 @@
-document.write("HOLA CHAVALES");
+
+// Ejercicio 1
+const idSol1 = document.getElementById('idSol1');
+const inputEjerc1 = document.getElementById('idEj1');
+
+function tablaMultiplicar(n) {
+
+    let tabla = "";
+
+    for (i = 1; i <= 10; i++) {
+
+        tabla = tabla + n + " x " + i + " = " + n * i + '<br/>';
+    }
+    return tabla;
+}
+
+function limpiar() {
+
+    idSol1.innerHTML = "";
+    inputEjerc1.value = "";
+}
+
+// Ejercicio 2
+const idSol2 = document.getElementById('idSol2');
+const inputEjerc2 = document.getElementById('idEj2');
+
+function cabeceras(cadena){
+
+    let str = "";
+
+    for(i = 1; i <= 6; i++){
+
+        str = str + '<h' + i + '>' + cadena.value + '</h' + i + '>' + '</br>'; 
+    }
+
+    return str;
+}
+
+
+
